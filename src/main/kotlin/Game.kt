24aa43bf4dfe8b272cae9f416940f9a7df87dbe0
Eduardo Alexandre <edubrdev@gmail.com>
2024-03-22 21:20:08 -0300

    feat: add Game Class

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,12 @@
+package org.example
+
+class Game {
+    var title = ""
+    var cover = ""
+    val description = ""
+    override fun toString(): String {
+        return "Game(title='$title', cover='$cover', description='$description')"
+    }
+
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,24 @@
 package org.example
 
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+
 fun main() {
-    println("Hello World!")
+    val client: HttpClient = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
+
+    val response = client
+        .send(request, BodyHandlers.ofString())
+
+    val json = response.body()
+    println(json)
+
+    val myGame = Game()
+    myGame.title = "Batman: Arkham Asylum Game of The Year Edition"
+    myGame.cover = "\"https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1702934705\""
+    println(myGame)
 }
